16752a1636cabee377da8eccf9046a765f44d3cb
CsarAlameda <dam_cesaralameda@riberadeltajo.es>
2023-02-14 16:03:36 +0100

    vuelvo a hacer commit

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
-		System.out.println("jeje");
+		System.out.println("jejefasdfasdfasd");
 
 	}
 
