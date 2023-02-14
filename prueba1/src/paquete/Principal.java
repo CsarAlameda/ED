1472b0726f587421985a0b30bc6cbe8565bd13b6
CsarAlameda <dam_cesaralameda@riberadeltajo.es>
2023-02-14 16:01:00 +0100

    hago commit

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -3,7 +3,9 @@ package paquete;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+		System.out.println("jeje");
 
 	}
 
